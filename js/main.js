712ecd25f3d66d3dd37903f3cd0d3ec246d085de
Gabriel da Silva <hello.gabrielsilva2@gmail.com>
2024-05-28 16:14:33 -0300

    novo layout

--- js/main.js
+++ js/main.js
@@ -22,16 +22,8 @@ events.forEach(event =>  {
   })
 })
 
-// VIDEO
-const btnVideo = document.querySelector(".page__header__top__video button")
-const modalVideo = document.querySelector(".video")
-const closeVideo = modalVideo.querySelector("button")
-
-btnVideo.addEventListener("click", () => modalVideo.classList.add("open"))
-closeVideo.addEventListener("click", () => modalVideo.classList.remove("open"))
-
 // BUTTONS
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll("[data-form]")
 const modal = document.querySelector(".modal")
 
 buttons.forEach(button => {
@@ -57,87 +49,183 @@ buttons.forEach(button => {
 modal.querySelector("[data-close]").addEventListener("click", () => modal.classList.remove("open"))
 
 // CASES
-gsap.registerPlugin(ScrollTrigger);
-
+const wrapper = document.querySelector(".page__cases__marquee__images");
 const boxes = gsap.utils.toArray(".page__cases__marquee__images__image");
-const boxes2 = gsap.utils.toArray(".page__emphasis__items__cases__case");
 
-const loop = horizontalLoop(boxes, { paused: false, repeat: -1 });
-const loop2 = horizontalLoop(boxes2, { paused: false, repeat: -1 });
+let activeElement;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loop = horizontalLoop(boxes, {
+    paused: false, 
+    draggable: true,
+    center: false,
+    onChange: (element, index) => {
+      activeElement && activeElement.classList.remove("-active");
+      element.classList.add("-active");
+      activeElement = element;
+    }
+  });
+})
 
 function horizontalLoop(items, config) {
-  items = gsap.utils.toArray(items);
-  config = config || {};
-  let tl = gsap.timeline({ repeat: config.repeat, paused: config.paused, defaults: { ease: "none" } }),
+	items = gsap.utils.toArray(items);
+	config = config || {};
+	let onChange = config.onChange,
+      lastIndex = 0,
+      tl = gsap.timeline({repeat: config.repeat, onUpdate: onChange && function() {
+        let i = tl.closestIndex()
+        if (lastIndex !== i) {
+          lastIndex = i;
+          onChange(items[i], i);
+        }
+      }, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 10)}),
       length = items.length,
       startX = items[0].offsetLeft,
       times = [],
       widths = [],
+      spaceBefore = [],
       xPercents = [],
       curIndex = 0,
-      snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1),
-      totalWidth, curX, distanceToStart, distanceToLoop, item, i;
-
-  // Função para calcular a distância a ser percorrida com base no scroll
-  function calculateDistance(scrollTop) {
-      // Aqui você pode ajustar o valor conforme necessário
-      return scrollTop * 0.05;
-  }
-
-  // Função para ajustar a posição dos itens com base no scroll
-  function updateScrollPosition() {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      const distance = calculateDistance(scrollTop);
-      tl.seek(distance); // Define a posição da animação com base na distância calculada
-  }
-
-  // Atualizar a posição dos itens ao rolar a página
-  window.addEventListener("scroll", updateScrollPosition);
-
-  gsap.set(items, {
-      xPercent: (i, el) => {
-          let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
-          xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
-          return xPercents[i];
-      }
-  });
-  gsap.set(items, { x: 0 });
-  totalWidth = items[length - 1].offsetLeft + xPercents[length - 1] / 100 * widths[length - 1] - startX + items[length - 1].offsetWidth * gsap.getProperty(items[length - 1], "scaleX") + (parseFloat(config.paddingRight) || 0);
-  for (i = 0; i < length; i++) {
-      item = items[i];
-      curX = xPercents[i] / 100 * widths[i];
-      distanceToStart = item.offsetLeft + curX - startX;
-      distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
-      // Removendo a referência a pixelsPerSecond
-      tl.to(item, { xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / 100 }, 0)
-          .fromTo(item, { xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100) }, { xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / 100, immediateRender: false }, distanceToLoop / 100)
-          .add("label" + i, distanceToStart / 100);
-      times[i] = distanceToStart / 100;
-  }
-  function toIndex(index, vars) {
-      vars = vars || {};
-      (Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
-      let newIndex = gsap.utils.wrap(0, length, index),
-          time = times[newIndex];
-      if (time > tl.time() !== index > curIndex) {
-          vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
-          time += tl.duration() * (index > curIndex ? 1 : -1);
-      }
-      curIndex = newIndex;
-      vars.overwrite = true;
-      return tl.tweenTo(time, vars);
-  }
-  tl.next = vars => toIndex(curIndex + 1, vars);
-  tl.previous = vars => toIndex(curIndex - 1, vars);
-  tl.current = () => curIndex;
-  tl.toIndex = (index, vars) => toIndex(index, vars);
-  tl.times = times;
+      center = config.center,
+      pixelsPerSecond = (config.speed || 1) * 100,
+      snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+      timeOffset = 0, 
+      container = center === true ? items[0].parentNode : gsap.utils.toArray(center)[0] || items[0].parentNode,
+      totalWidth,
+      getTotalWidth = () => items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + spaceBefore[0] + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0),
+      populateWidths = () => {
+        let b1 = container.getBoundingClientRect(), b2;
+        items.forEach((el, i) => {
+          widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+          xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / widths[i] * 100 + gsap.getProperty(el, "xPercent"));
+          b2 = el.getBoundingClientRect();
+          spaceBefore[i] = b2.left - (i ? b1.right : b1.left);
+          b1 = b2;
+        });
+        gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+          xPercent: i => xPercents[i]
+        });
+        totalWidth = getTotalWidth();
+      },
+      timeWrap,
+      populateOffsets = () => {
+        timeOffset = center ? tl.duration() * (container.offsetWidth / 2) / totalWidth : 0;
+        center && times.forEach((t, i) => {
+          times[i] = timeWrap(tl.labels["label" + i] + tl.duration() * widths[i] / 2 / totalWidth - timeOffset);
+        });
+      },
+      getClosest = (values, value, wrap) => {
+        let i = values.length,
+          closest = 1e10,
+          index = 0, d;
+        while (i--) {
+          d = Math.abs(values[i] - value);
+          if (d > wrap / 2) {
+            d = wrap - d;
+          }
+          if (d < closest) {
+            closest = d;
+            index = i;
+          }
+        }
+        return index;
+      },
+      populateTimeline = () => {
+        let i, item, curX, distanceToStart, distanceToLoop;
+        tl.clear();
+        for (i = 0; i < length; i++) {
+          item = items[i];
+          curX = xPercents[i] / 100 * widths[i];
+          distanceToStart = item.offsetLeft + curX - startX + spaceBefore[0];
+          distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+          tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+            .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+            .add("label" + i, distanceToStart / pixelsPerSecond);    
+          times[i] = distanceToStart / pixelsPerSecond;
+        }
+        timeWrap = gsap.utils.wrap(0, tl.duration());
+      }, 
+      refresh = (deep) => {
+         let progress = tl.progress();
+         tl.progress(0, true);
+         populateWidths();
+         deep && populateTimeline();
+         populateOffsets();
+         deep && tl.draggable ? tl.time(times[curIndex], true) : tl.progress(progress, true);
+      },
+      proxy;
+	gsap.set(items, {x: 0});
+  populateWidths();
+	populateTimeline();
+  populateOffsets();
+  window.addEventListener("resize", () => refresh(true));
+	function toIndex(index, vars) {
+		vars = vars || {};
+		(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
+		let newIndex = gsap.utils.wrap(0, length, index),
+			time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
+			time += tl.duration() * (index > curIndex ? 1 : -1);
+		}
+    if (time < 0 || time > tl.duration()) {
+      vars.modifiers = {time: timeWrap};
+    }
+		curIndex = newIndex;
+		vars.overwrite = true;
+    gsap.killTweensOf(proxy);
+		return tl.tweenTo(time, vars);
+	}
+	tl.next = vars => toIndex(curIndex+1, vars);
+	tl.previous = vars => toIndex(curIndex-1, vars);
+	tl.current = () => curIndex;
+	tl.toIndex = (index, vars) => toIndex(index, vars);
+  tl.closestIndex = setCurrent => {
+    let index = getClosest(times, tl.time(), tl.duration());
+    setCurrent && (curIndex = index);
+    return index;
+  };
+	tl.times = times;
   tl.progress(1, true).progress(0, true); // pre-render for performance
   if (config.reversed) {
-      tl.vars.onReverseComplete();
-      tl.reverse();
+    tl.vars.onReverseComplete();
+    tl.reverse();
   }
-  return tl;
+  if (config.draggable && typeof(Draggable) === "function") {
+    proxy = document.createElement("div")
+    let wrap = gsap.utils.wrap(0, 1),
+        ratio, startProgress, draggable, dragSnap,
+        align = () => tl.progress(wrap(startProgress + (draggable.startX - draggable.x) * ratio)),
+        syncIndex = () => tl.closestIndex(true);
+    typeof(InertiaPlugin) === "undefined" && console.warn("InertiaPlugin required for momentum-based scrolling and snapping. https://greensock.com/club");
+    draggable = Draggable.create(proxy, {
+      trigger: items[0].parentNode,
+      type: "x",
+      onPressInit() {
+        gsap.killTweensOf(tl);
+        startProgress = tl.progress();
+        refresh();
+        ratio = 1 / totalWidth;
+        gsap.set(proxy, {x: startProgress / -ratio})
+      },
+      onDrag: align,
+      onThrowUpdate: align,
+      inertia: true,
+      snap: value => {
+        let time = -(value * ratio) * tl.duration(),
+            wrappedTime = timeWrap(time),
+            snapTime = times[getClosest(times, wrappedTime, tl.duration())],
+            dif = snapTime - wrappedTime;
+        Math.abs(dif) > tl.duration() / 2 && (dif += dif < 0 ? tl.duration() : -tl.duration());
+        return (time + dif) / tl.duration() / -ratio;
+      },
+      onRelease: syncIndex,
+      onThrowComplete: syncIndex
+    })[0];
+    tl.draggable = draggable;
+  }
+  tl.closestIndex(true);
+  onChange && onChange(items[curIndex], curIndex);
+	return tl;
 }
 
 // EXPANSÍVEIS
@@ -187,19 +275,6 @@ function AnimationTitles(title, elem) {
 }
 
 AnimationTitles("[data-title]", "[data-title] div")
-
-// QUESTIONS
-new Swiper(".page__questions .swiper", {
-  effect: "fade",
-  navigation: {
-    prevEl: ".page__questions [data-prev]",
-    nextEl: ".page__questions [data-next]"
-  },
-  pagination: {
-    el: ".page__questions [data-pagination]",
-    clickable: true
-  }
-})
 
 // BACK TO TOP
 const btnTop = document.querySelector("[data-top]")
